3362be062bcbc735f5f8bb353d0a2c019fa9cb5c
Freddie Nelson <freddie0208@hotmail.com>
2022-09-06 21:35:00 +0100

    post video example

--- examples/postSlideshow.ts
+++ examples/postSlideshow.ts
@@ -5,9 +5,12 @@ const postSlideshow: Example = async (username, password) => {
   const bot = new IGBot(username, password);
   await bot.init();
   await bot.login();
-  await bot.post(["../test-post-image-0.jpg", "test-post-image-1.jfif", "test-post-image-2.jpg"], {
-    caption: "This is a slideshow post.",
-  });
+  await bot.post(
+    ["../test-post-image-0.jpg", "../test-post-image-1.jfif", "../test-post-image-2.jpg"],
+    {
+      caption: "This is a slideshow post.",
+    },
+  );
   await bot.close();
 };
 
---
+++ examples/postVideo.ts
@@ -0,0 +1,14 @@
+import IGBot from "@/igBot";
+import { Example } from "./example";
+
+const postVideo: Example = async (username, password) => {
+  const bot = new IGBot(username, password);
+  await bot.init();
+  await bot.login();
+  await bot.post("../test-post-video-0.mp4", {
+    caption: "This is a slideshow post.",
+  });
+  await bot.close();
+};
+
+export default postVideo;
